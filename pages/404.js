4588fb0f769a26f95c6d92c3c34329b9e5d0aaf8
lucasdgimenez <lucas_d360@hotmail.com>
2021-02-13 13:20:22 -0300

    aula 02

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function Page404() {
+  return (
+    <div>
+      Pagina 404
+    </div>
+  )
+}
---
+++ pages/sobre.js
@@ -0,0 +1,7 @@
+export default function PageSobre() {
+  return (
+    <div>
+      Pagina sobre
+    </div>
+  )
+}
