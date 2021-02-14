a1fe5441fa0dc32f3cb2ebb959be096c8c531449
lucasdgimenez <lucas_d360@hotmail.com>
2021-02-14 00:19:14 -0300

    estruturando menu

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,9 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
-const Text = styled.p`
-  font-size: 20px;
-  color: black;
-`
-
-function Button() {
-  return <button>Cadastrar</button>
-}
+import Menu from "../src/components/commons/Menu"
 
 export default function Home() {
-  return <div>
-  <Title>My page</Title>
-  <Text>Bla bla</Text>
-  <Button/>
-  </div> 
+  return (
+    <div>
+      <Menu/>
+    </div> 
+  )
 }
