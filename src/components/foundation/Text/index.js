cfe104c1f7a0b1a6278c3e69c5c49ba3ef30d821
lucasdgimenez <lucas_d360@hotmail.com>
2021-02-21 21:32:34 -0300

    exportando os padroes de tipografia, criacao do componente padrao de texto e definindo o texto do menu

---
+++ src/components/foundation/Text/index.js
@@ -0,0 +1,29 @@
+import React from "react"
+import styled, {css} from "styled-components"
+
+const TextBase = styled.span`
+  ${function(props) {
+    if(props.variant === 'smallestException')
+    return css`
+      font-size: ${({ theme }) => theme.typographyVariants.smallestException.fontSize};
+      font-weight:  ${({theme}) => theme.typographyVariants.smallestException.fontWeight};
+      line-height: ${({theme}) => theme.typographyVariants.smallestException.lineHeight};
+    `
+  }}
+`
+
+export default function Text({tag, variant, children}) {
+  return (
+    <TextBase
+      as={tag}
+      variant={variant}
+    >
+    {children}
+  </TextBase>
+  )
+}
+
+Text.defaultProps = {
+  tag: 'span',
+  variant: 'paragraph1'
+}
